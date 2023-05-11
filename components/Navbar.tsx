c53e0f5a23a0eeb349de79914aeb14352bc3961f
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-11 23:47:06 +0530

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-scroll/modules";
 import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 import { RiMoonFill, RiSunLine } from "react-icons/ri";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
+import Image from "next/image";
 
 interface NavItem {
   label: string;
@@ -29,17 +30,26 @@ const NAV_ITEMS: Array<NavItem> = [
 
 const Navbar = () => {
   const { systemTheme, theme, setTheme } = useTheme();
-  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [mounted, setMounted] = useState(false);
   const pathname = usePathname();
-  const [navbar, setNavbar] = useState(false);
+  const [navbar, setNavbar] = useState(true);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+
   return (
-    <header className="fixed top-0 z-50 w-full px-4 mx-auto bg-white shadow sm:px-20 dark:bg-stone-900 dark:border-b dark:border-stone-600">
-      <div className="justify-between md:items-center md:flex">
+    <header className="fixed top-0 z-50 w-full px-4 mx-auto bg-white sm:px-20 dark:bg-stone-900">
+      <div className="justify-between lg:justify-around md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link to="home">
               <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Yashwant Gawande</h2>
+                <h2 className="text-2xl font-bold">Yash</h2>
               </div>
             </Link>
             <div className="md:hidden">
@@ -66,7 +76,7 @@ const Navbar = () => {
                     key={idx}
                     to={item.page}
                     className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100 cursor-pointer"
                     }
                     activeClass="active"
                     spy={true}
@@ -85,13 +95,15 @@ const Navbar = () => {
                   className="p-2 bg-slate-100 rounded-xl"
                 >
                   <RiSunLine size={25} color="black" />
+                  {/* <Image src="/assets/sun.svg" alt="light-mode" height={25} width={25} /> */}
                 </button>
               ) : (
                 <button
                   onClick={() => setTheme("dark")}
                   className="p-2 bg-slate-100 rounded-xl"
                 >
-                  <RiMoonFill size={25} />
+                  <RiMoonFill size={25} color="black" />
+                  {/* <Image src="/assets/moon.svg" alt="dark-mode" height={25} width={25} /> */}
                 </button>
               )}
             </div>
