458c96900248321045e50d6fc20d72299c23704f
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-15 23:38:43 +0530

    Add background to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AboutSection from "@/components/AboutSection"
+import Background from "@/components/Background"
 import HeroSection from "@/components/HeroSection"
 import ProjectsSection from "@/components/ProjectsSection"
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4 sm:px-6">
+      <Background/>
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
