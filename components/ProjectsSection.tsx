26bc85df1d56c324416d756ee4f7574896a38b7d
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-19 00:01:14 +0530

    update project section

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -16,26 +16,43 @@ const PROJECTS: Array<Projects> = [
     desc: "Introducing an AI-powered blog app that revolutionizes content creation. Harnessing the power of artificial intelligence, this app generates high-quality blog articles effortlessly, saving you time and effort. ",
     tech: ["React", "Next.js", "Tailwind css"],
   },
+  {
+    name: "Ai blog app",
+    img: "/assets/ai-1.jpg",
+    desc: "Introducing an AI-powered blog app that revolutionizes content creation. Harnessing the power of artificial intelligence, this app generates high-quality blog articles effortlessly, saving you time and effort. ",
+    tech: ["React", "Next.js", "Tailwind css"],
+  },
+  {
+    name: "Ai blog app",
+    img: "/assets/ai-1.jpg",
+    desc: "Introducing an AI-powered blog app that revolutionizes content creation. Harnessing the power of artificial intelligence, this app generates high-quality blog articles effortlessly, saving you time and effort. ",
+    tech: ["React", "Next.js", "Tailwind css"],
+  },
 ];
 
 const ProjectsSection: React.FC = () => {
   return (
-    <section id="projects" className="container p-4 mx-auto lg:p-8 xl:px-0 ">
+    <section
+      id="projects"
+      className="container p-4 mx-auto rounded-lg lg:p-8 xl:px-0 "
+    >
       <div className="flex flex-col lg:flex-row">
-        <div className="p-4 lg:w-1/2 lg:p-8">
-          <h1 className="p-2 text-4xl font-bold text-center lg:p-4 xl:text-6xl">
+        <div className="flex flex-col items-center p-4 lg:w-1/2 lg:p-8">
+          <h1 className="p-2 mb-4 text-2xl font-bold text-center lg:p-4 xl:text-4xl">
             Projects
           </h1>
-          <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-            A curated collection of my standout projects showcasing expertise in
-            web development, mobile apps, and more. From stunning designs to
-            seamless user experiences, each project solves unique problems using
-            cutting-edge technologies. Browse brief overviews, key features, and
-            interactive demos, and feel free to connect for collaboration or
-            inquiries. Welcome to innovation and excellence in action!
-          </p>
+          <div className="w-full h-auto md:w-[70%]">
+            <Image
+              src="/assets/app-launch.svg"
+              width={616}
+              height={617}
+              style={{ objectFit: "cover" }}
+              alt="Hero Illustration"
+              priority={false}
+            />
+          </div>
         </div>
-        <div className="p-4 lg:w-1/2 lg:p-8">
+        <div className="flex flex-col gap-1 p-4 lg:w-1/2 lg:p-8">
           {PROJECTS.map((item, id) => {
             return (
               <div
@@ -45,7 +62,7 @@ const ProjectsSection: React.FC = () => {
                 <div className="flex items-center gap-5 mb-5 text-3xl font-bold text-gray-900 dark:text-white">
                   {item.name}
                 </div>
-                <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+                <p className="mb-5 text-base text-gray-800 sm:text-lg dark:text-gray-100">
                   {item.desc}
                 </p>
                 <div className="flex flex-wrap">
@@ -53,14 +70,17 @@ const ProjectsSection: React.FC = () => {
                     return (
                       <span
                         key={id}
-                        className="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
+                        className="dark:bg-primary-purple bg-secondary-pink font-medium border rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
                       >
                         {tech}
                       </span>
                     );
                   })}
                 </div>
-                <div className="absolute inset-0 transition-opacity duration-300 bg-center bg-cover rounded-lg opacity-0 -z-1 hover:opacity-10" style={{ backgroundImage: `url(${item.img})` }}></div>
+                <div
+                  className="absolute inset-0 transition-opacity duration-300 bg-center bg-cover rounded-lg opacity-0 hover:opacity-20 dark:hover:opacity-10"
+                  style={{ backgroundImage: `url(${item.img})` }}
+                ></div>
               </div>
             );
           })}
