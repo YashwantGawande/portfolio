1139a4be7d547097e5615ad5cf9aafcc62575115
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-19 00:00:18 +0530

    refractor navbar style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,26 +35,26 @@ const Navbar = () => {
       <nav className="container px-8 mx-auto md:flex md:items-center xl:px-0">
         <div className="flex items-center justify-between w-full py-3 md:py-5 md:block">
           <Link to="home">
-            <button className="container flex items-center space-x-2">
+            <div className="container flex items-center space-x-2 cursor-pointer">
               <h2 className="text-2xl font-bold">Yash</h2>
-            </button>
+            </div>
           </Link>
           <div className="md:hidden">
             <button
               className="p-2 rounded-md outline-none focus:border-gray-400 focus:border"
               onClick={() => setNavbar(!navbar)}
             >
-              {navbar ? <IoMdClose size={30}  /> : <IoMdMenu size={30}/>}
+              {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
             </button>
           </div>
         </div>
 
         <div
-          className={`mt-8 md:flex md:pb-0 md:mt-0 ${
+          className={`md:flex md:pb-0 md:mt-0 absolute md:static w-[87%] ${
             navbar ? "flex" : "hidden"
           }`}
         >
-          <div className="items-start justify-center w-full p-5 space-y-8 rounded-lg bg-stone-900/30 md:bg-transparent md:flex md:space-x-6 md:space-y-0">
+          <div className="items-center justify-center w-full p-5 space-y-8 bg-white bg-opacity-50 rounded-lg backdrop-blur-md drop-shadow-lg md:bg-transparent md:flex md:space-x-6 md:space-y-0">
             {NAV_ITEMS.map((item, idx) => {
               return (
                 <Link
@@ -65,12 +65,10 @@ const Navbar = () => {
                   smooth={true}
                   offset={-100}
                   duration={500}
-                  className={`flex w-full cursor-pointer items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-primary-purple to-secondary-pink group-hover:from-primary-purple group-hover:to-secondary-pink hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-secondary-pink dark:focus:ring-purple-800`}
+                  className={`w-full flex cursor-pointer items-center justify-start md:justify-center bg-clip-text font-semibold hover:text-transparent bg-gradient-to-r from-primary-purple to-secondary-pink`}
                   onClick={() => setNavbar(!navbar)}
                 >
-                  <span className="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-stone-900 rounded-md group-hover:bg-opacity-0">
-                    {item.label}
-                  </span>
+                  {item.label}
                 </Link>
               );
             })}
