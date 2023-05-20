3b2c8a304fecfc4c4b16fbb14c7b29eba2a54919
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-21 00:11:07 +0530

    update page section with new added section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import AboutSection from "@/components/AboutSection"
+import SkillSection from "@/components/SkillSection"
 import Background from "@/components/Background"
 import HeroSection from "@/components/HeroSection"
 import ProjectsSection from "@/components/ProjectsSection"
+import AboutMeSection from "@/components/AboutMeSection"
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <Background/>
       <HeroSection/>
-      <AboutSection />
+      <SkillSection />
       <ProjectsSection />
+      <AboutMeSection/>
     </main>
   )
 }
