51f7f865709a1ff27e740c0abf8d72a378c90399
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-27 23:02:53 +0530

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import SkillSection from "@/components/SkillSection"
-import Background from "@/components/Background"
-import HeroSection from "@/components/HeroSection"
-import ProjectsSection from "@/components/ProjectsSection"
-import AboutMeSection from "@/components/AboutMeSection"
+import SkillSection from "@/components/SkillSection";
+import Background from "@/components/Background";
+import HeroSection from "@/components/HeroSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import AboutMeSection from "@/components/AboutMeSection";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <Background/>
-      <HeroSection/>
+      <Background />
+      <HeroSection />
       <SkillSection />
       <ProjectsSection />
-      <AboutMeSection/>
+      <AboutMeSection />
     </main>
-  )
+  );
 }
