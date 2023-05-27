cdd048c767330251bfcf295f8472b25320f77d21
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-27 23:09:42 +0530

    update skills section

--- components/SkillSection.tsx
+++ components/SkillSection.tsx
@@ -1,8 +1,23 @@
+"use client";
+import Image from "next/image";
 import React from "react";
-import { IoLogoHtml5, IoLogoCss3, IoLogoJavascript } from "react-icons/io";
-import { SiTypescript, SiReact, SiMongodb, SiPostgresql } from "react-icons/si";
+import {
+  IoLogoHtml5,
+  IoLogoCss3,
+  IoLogoJavascript,
+  IoLogoGithub,
+  IoLogoNodejs,
+} from "react-icons/io";
+import {
+  SiTypescript,
+  SiReact,
+  SiMongodb,
+  SiMysql,
+  SiTailwindcss,
+} from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import Tilt from "react-parallax-tilt";
+import "@/styles/ParallaxEffectGlareScale.css";
 
 interface SkillItem {
   skill: string;
@@ -39,39 +54,52 @@ const SKILL_ITEMS: Array<SkillItem> = [
     icon: <SiMongodb />,
   },
   {
-    skill: "Postgresql",
-    icon: <SiPostgresql />,
+    skill: "MySQL",
+    icon: <SiMysql />,
   },
-  // { skill: "Tailwind CSS", icon: "", desc: "" },
-  // { skill: "GitHub", icon: "", desc: "" },
-  // { skill: "Node.js", icon: "", desc: "" },
+  { skill: "Tailwind CSS", icon: <SiTailwindcss /> },
+  { skill: "GitHub", icon: <IoLogoGithub /> },
+  { skill: "Node.js", icon: <IoLogoNodejs /> },
 ];
 
-const AboutSection = () => {
+const SkillSection = () => {
   return (
     <section
-      id="about"
-      className="container p-4 mx-auto mt-4 bg-black rounded-lg lg:p-8 xl:px-0 bg-opacity-10"
+      id="skills"
+      className="container flex flex-col p-5 mx-auto bg-black rounded-lg lg:h-screen lg:items-center lg:p-8 xl:px-0 bg-opacity-10 lg:flex-row"
     >
-      <h1 className="p-2 mb-4 text-2xl font-bold text-center lg:p-4 xl:text-4xl">
-        Skills
-      </h1>
-      <div className="flex flex-wrap justify-center gap-5 pt-4">
+      <div className="lg:w-1/2">
+        <h1
+          className={`font-source p-2 my-4 text-center lg:p-4 text-4xl md:text-6xl lg:text-8xl`}
+        >
+          Skills
+        </h1>
+        <div className="flex justify-center w-full h-auto">
+          <Image
+            src="/assets/engineer.svg"
+            width={616}
+            height={617}
+            style={{ objectFit: "cover" }}
+            alt="Hero Illustration"
+            priority={false}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-wrap items-center justify-center p-4 h-min lg:w-1/2">
         {SKILL_ITEMS.map((item, id) => {
           return (
             <Tilt
+              className="p-4 text-base text-center bg-white border border-gray-200 shadow cursor-default xl:p-5 xl:text-lg parallax-effect-glare-scale hover:shadow-primary-purple/50 dark:bg-stone-900/30 dark:border-gray-700"
               glareEnable={true}
               glareMaxOpacity={0.8}
               glareColor="#ffffff"
               glarePosition="bottom"
-              glareBorderRadius="20px"
+              // glareBorderRadius="8px"
               key={id}
             >
-              <div
-                
-                className="p-4 text-center bg-white border border-gray-200 rounded-lg shadow hover:shadow-primary-purple/50 sm:p-8 dark:bg-stone-900/30 dark:border-gray-700"
-              >
-                <div className="flex items-center gap-5 text-3xl font-bold text-gray-900 dark:text-white">
+              <div className="inner-element">
+                <div className="flex items-center gap-2 font-bold text-gray-900 dark:text-white">
                   {item.icon}
                   {item.skill}
                 </div>
@@ -87,4 +115,4 @@ const AboutSection = () => {
   );
 };
 
-export default AboutSection;
+export default SkillSection;
