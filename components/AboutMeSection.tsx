0a085bdd30e52f2271a68489f9ab437e4b4d2419
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-28 23:54:48 +0530

    add content to about section

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -28,23 +28,44 @@ const LINKS: Array<Links> = [
 const AboutMeSection = () => {
   return (
     <section id="about" className="container h-screen p-5 mt-4 rounded-lg">
-      <div className="flex flex-col gap-5">
-        <div className="lg:w-1/2 ">
+      <div className="flex flex-col gap-5 lg:flex-row">
+        <div className="lg:w-1/2">
           <h1 className="pb-5 text-4xl font-source">Who&#39;s Yash?</h1>
           <p className="text-lg">
             Hi there 👋 My name is Yashwant Gawande. I am Full-Stack Developer
-            based in india. 
+            based in india.
+          </p>
+          <br />
+          <p className="text-lg">
+            I&#39;m a Full Stack Developer experienced in creating innovative
+            web applications. With expertise in both front-end and back-end
+            development, I design visually appealing interfaces and ensure
+            efficient data management. My goal is to deliver feature-rich web
+            applications that make a lasting impression.
+          </p>
+          <br />
+          <p className="text-lg">
+            My approach to development goes beyond just writing code. I strive
+            to understand the bigger picture and the problem that needs to be
+            solved. By collaborating closely with clients and stakeholders, I
+            can provide valuable insights and create solutions that meet their
+            unique needs.
+          </p>
+          <br />
+          <p className="text-lg">
+            Thank you for visiting my portfolio website. Feel free to explore my
+            projects and get in touch if you have any exciting opportunities or
+            ideas to discuss. Let&#39;s create something amazing together!
           </p>
         </div>
-        <div>another content</div>
+        <div className="flex flex-col justify-center lg:w-1/2">
+          <h3 className="text-2xl text-center">Contact me</h3>
+          <ContactForm />
+        </div>
       </div>
-      <div className="flex flex-col gap-5 my-5">
-        <h3 className="text-2xl text-center">Contact me</h3>
-        <ContactForm />
-      </div>
-      <div className="py-5">
-        <h3 className="text-2xl">Find Me</h3>
-        <ul>
+      <div className="flex flex-col">
+        <h3 className="text-2xl text-center">Find Me</h3>
+        <ul className="">
           {LINKS.map((item, id) => {
             return (
               <li key={id} className="flex items-center gap-2">
