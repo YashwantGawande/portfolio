9851e4da7b6cd6b10fa7f6866370a815ec07f23b
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-30 23:31:29 +0530

    style update

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { Caveat, Roboto_Mono, Work_Sans } from "next/font/google";
 import Image from "next/image";
 import React from "react";
-import Tilt from "react-parallax-tilt";
 
 interface Projects {
   name: string;
@@ -58,57 +56,58 @@ const PROJECTS: Array<Projects> = [
 
 const ProjectsSection: React.FC = () => {
   return (
-    <section
-      id="projects"
-      className="container grid justify-center grid-rows-1 mx-auto mt-4 rounded-lg bg-black/10 lg:grid-cols-2 lg:p-8 xl:px-0"
-    >
-      <div>
-        <div className="lg:sticky top-16">
-          <h1 className="p-2 my-4 text-4xl text-center font-source lg:p-4 md:text-6xl lg:text-8xl">
-            Projects
-          </h1>
-          <div className="flex justify-center w-full h-auto">
-            <Image
-              src="/assets/app-launch.svg"
-              width={616}
-              height={617}
-              style={{ objectFit: "cover" }}
-              alt="Hero Illustration"
-              priority={false}
-            />
+    <section id="projects" className="container">
+      <h1 className="p-2 my-4 text-4xl text-center font-source lg:p-4 md:text-6xl lg:text-8xl">
+        Projects
+      </h1>
+      <div className="grid justify-center grid-rows-1 mx-auto mt-4 lg:grid-cols-2 lg:p-8 xl:px-0">
+        <div>
+          <div className="lg:sticky top-16">
+            <div className="flex justify-center w-full h-auto">
+              <Image
+                src="/assets/app-launch.svg"
+                width={616}
+                height={617}
+                style={{ objectFit: "cover" }}
+                alt="Hero Illustration"
+                priority={false}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="my-4">
-        {PROJECTS.map((item, id) => {
-          return (
-            <div
-              key={id}
-              className="relative p-4 m-5 text-center bg-white border border-gray-200 rounded-lg shadow z-1 hover:shadow-primary-purple/50 sm:p-8 dark:bg-stone-900/30 dark:border-gray-700"
-            >
-              <div className="mb-5 text-3xl text-left font-source">{item.name}</div>
-              <p className="mb-5 text-left text-gray-800 dark:text-gray-100">
-                {item.desc}
-              </p>
-              <div className="flex flex-wrap">
-                {item.tech.map((tech, id) => {
-                  return (
-                    <span
-                      key={id}
-                      className="dark:bg-primary-purple bg-secondary-pink font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                    >
-                      {tech}
-                    </span>
-                  );
-                })}
-              </div>
+        <div className="">
+          {PROJECTS.map((item, id) => {
+            return (
               <div
-                className="absolute inset-0 transition-opacity duration-300 bg-center bg-cover rounded-lg opacity-0 hover:opacity-20 dark:hover:opacity-10"
-                style={{ backgroundImage: `url(${item.img})` }}
-              ></div>
-            </div>
-          );
-        })}
+                key={id}
+                className="relative p-4 m-5 text-center bg-white border border-gray-200 rounded-lg shadow z-1 hover:shadow-primary-purple/50 sm:p-8 dark:bg-stone-900/30 dark:border-gray-700"
+              >
+                <div className="mb-5 text-3xl text-left font-source">
+                  {item.name}
+                </div>
+                <p className="mb-5 text-left text-gray-800 dark:text-gray-100">
+                  {item.desc}
+                </p>
+                <div className="flex flex-wrap">
+                  {item.tech.map((tech, id) => {
+                    return (
+                      <span
+                        key={id}
+                        className="dark:bg-primary-purple bg-secondary-pink font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                      >
+                        {tech}
+                      </span>
+                    );
+                  })}
+                </div>
+                <div
+                  className="absolute inset-0 transition-opacity duration-300 bg-center bg-cover rounded-lg opacity-0 hover:opacity-20 dark:hover:opacity-10"
+                  style={{ backgroundImage: `url(${item.img})` }}
+                ></div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
