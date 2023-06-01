03c5e1be113e00fdafbc2a070394cac028464c5a
YashwantGawande <gawandeyashwant7@gmail.com>
2023-06-01 23:36:59 +0530

    add animation with framer motion

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -7,6 +7,7 @@ import { ToastContainer, toast } from "react-toastify";
 import Confetti from "react-confetti";
 
 import { validationSchema } from "@/utils/validations";
+import { motion } from "framer-motion";
 
 type FormValues = {
   name: string;
@@ -46,7 +47,7 @@ const ContactForm = () => {
       // Show success message or redirect to a thank you page
       toast.success("Form submitted successfully!");
       setShowConfetti(true);
-      console.log(showConfetti)
+      console.log(showConfetti);
       console.log("Email sent successfully!");
     } catch (error) {
       // Handle error
@@ -69,7 +70,17 @@ const ContactForm = () => {
           <div className="mx-auto lg:w-2/3">
             <div className="flex flex-wrap">
               <div className="w-1/2 p-2">
-                <div className="relative">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  transition={{ duration: 1 }}
+                  variants={{
+                    visible: { opacity: 1, y: 0 },
+                    hidden: { opacity: 0, y: 100 },
+                  }}
+                  className="relative"
+                >
                   <label className="text-sm leading-7 md:text-lg">Name</label>
                   <Field
                     type="text"
@@ -82,10 +93,20 @@ const ContactForm = () => {
                     component="div"
                     className="text-red-500"
                   />
-                </div>
+                </motion.div>
               </div>
               <div className="w-1/2 p-2">
-                <div className="relative">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  transition={{ duration: 1 }}
+                  variants={{
+                    visible: { opacity: 1, y: 0 },
+                    hidden: { opacity: 0, y: 100 },
+                  }}
+                  className="relative"
+                >
                   <label className="text-sm leading-7 md:text-lg">Email</label>
                   <Field
                     type="email"
@@ -98,11 +119,23 @@ const ContactForm = () => {
                     component="div"
                     className="text-red-500"
                   />
-                </div>
+                </motion.div>
               </div>
               <div className="w-full p-2">
-                <div className="relative">
-                  <label className="text-sm leading-7 md:text-lg">Message</label>
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  transition={{ duration: 1 }}
+                  variants={{
+                    visible: { opacity: 1, y: 0 },
+                    hidden: { opacity: 0, y: 100 },
+                  }}
+                  className="relative"
+                >
+                  <label className="text-sm leading-7 md:text-lg">
+                    Message
+                  </label>
                   <Field
                     id="message"
                     name="message"
@@ -114,16 +147,53 @@ const ContactForm = () => {
                     component="div"
                     className="text-red-500"
                   />
-                </div>
+                </motion.div>
               </div>
-              <div className="w-full p-2">
-                <button
-                  disabled={isLoading}
-                  className="flex px-8 py-2 mx-auto text-lg text-white border-0 rounded bg-primary-purple focus:outline-none hover:bg-secondary-pink"
-                >
-                  Button
-                </button>
-              </div>
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 1 }}
+                variants={{
+                  visible: { opacity: 1, y: 0 },
+                  hidden: { opacity: 0, y: 100 },
+                }}
+                className="flex items-center justify-center w-full p-2"
+              >
+                {isLoading ? (
+                  <button
+                    disabled
+                    type="button"
+                    className="text-white bg-primary-purple hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      role="status"
+                      className="inline w-4 h-4 mr-3 text-white animate-spin"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="#E5E7EB"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    Loading...
+                  </button>
+                ) : (
+                  <button
+                    disabled={isLoading}
+                    className="text-white bg-primary-purple hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center"
+                  >
+                    Button
+                  </button>
+                )}
+              </motion.div>
             </div>
           </div>
         </Form>
@@ -142,7 +212,11 @@ const ContactForm = () => {
       />
 
       {showConfetti && (
-        <Confetti width={window.innerWidth} height={window.innerHeight} recycle={false} />
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+        />
       )}
     </>
   );
